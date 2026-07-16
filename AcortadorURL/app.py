ab2824c2b0e4779ef630015d2819d378751be673
CynthiaCarinoTrujillo <cynthia.carinot@gmail.com>
2026-07-16 22:06:37 +0200

    Index.html agregado dentro de las plantillas

--- AcortadorURL/app.py
+++ AcortadorURL/app.py
@@ -1,11 +1,12 @@
-from flask import Flask #clase importada
+from flask import Flask, render_template #clase importada
 
 app= Flask(__name__) #inicializando aplicacion
 
 @app.route('/') #esto es un decorador indicando que es la ruta raiz
 
 def index(): #esto es una vista que se expresa en forma de funcion
-    return "Hola :)"
+    #return "<h1>Hola :)<h1>"
+    return render_template('index.html')
 
 if __name__=='__main__': #comprobamos que si estamos desde el archivo main 
     app.run(debug=True, port=5000)  #entonces ejecutamos la aplicacion
