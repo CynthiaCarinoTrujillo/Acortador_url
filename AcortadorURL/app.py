62ff9d16cfb75d84952d319087db4e51d2677160
CynthiaCarinoTrujillo <cynthia.carinot@gmail.com>
2026-07-17 13:15:35 +0200

    Refactor app.py to implement URL shortening logic and enhance index.html comments for clarity

--- AcortadorURL/app.py
+++ AcortadorURL/app.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template #clase importada
+import secrets
+from flask import Flask, render_template, request, redirect, abort
 
-app= Flask(__name__) #inicializando aplicacion
+app = Flask(__name__)
 
-@app.route('/') #esto es un decorador indicando que es la ruta raiz
+short_urls = {}
 
-def index(): #esto es una vista que se expresa en forma de funcion
-    #return "<h1>Hola :)<h1>"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        url = request.form['URLusuario']
+        short_id = secrets.token_urlsafe(4)
+        short_urls[short_id] = url
+        short_url = request.host_url.rstrip('/') + '/' + short_id
+        return render_template('index.html', short_url=short_url, original_url=url)
     return render_template('index.html')
 
-if __name__=='__main__': #comprobamos que si estamos desde el archivo main 
-    app.run(debug=True, port=5000)  #entonces ejecutamos la aplicacion
+@app.route('/<short_id>')
+def redirect_short_url(short_id):
+    if short_id in short_urls:
+        return redirect(short_urls[short_id])
+    return abort(404)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
